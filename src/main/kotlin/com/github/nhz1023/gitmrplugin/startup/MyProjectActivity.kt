2395b77d1920e4aae57b3f5b2e6d7a1c072f33b1
GitHub Action <action@github.com>
2025-11-23 09:05:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/nhz1023/gitmrplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/nhz1023/gitmrplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.nhz1023.gitmrplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
